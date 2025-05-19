423041ada56c876e8448a98367bc28e289ce1c04
Programer-Turtle <karsonulerick@gmail.com>
2025-05-19 00:01:43 -0400

    Added EOTS in UI

--- Scripts/UILoad.js
+++ Scripts/UILoad.js
@@ -2,7 +2,7 @@ async function AdjustNavText() {
   let BodySize = document.body.clientWidth;
   let FontSize = null;
   if (BodySize <= 445) {
-    FontSize = "20px";
+    FontSize = "18px";
   } else if (BodySize <= 500) {
     FontSize = "25px";
   } else {
@@ -18,7 +18,7 @@ async function LoadNavBar() {
   try {
     document.getElementById(
       "NavBar"
-    ).innerHTML = `<a style="padding: 0; margin: 0; margin-top: 5px; margin-left: 10px; margin-right: 40px;"href="index.html"><img style="padding: 0; margin: 0;" width="80px" src="Images/Logo.webp" alt="Logo"></a><a class="NavButton" href="Search.html">Search</a><a style="margin-left:auto; margin-right:20px;" id="SigninButton" class="NavButton" href="signin.html">Sign In</a><a style="margin-right:20px;" id="SignupButton" class="NavButton" href="signup.html">Sign Up</a><a style="margin-left:auto; margin-right:20px; display: none;" id="AccountButton" class="NavButton" href="account.html">Account</a><a style="margin-right:20px; display: none;" id="ServiceButton" class="NavButton" href="services.html">Services</a>`;
+    ).innerHTML = `<a style="padding: 0; margin: 0; margin-top: 5px; margin-left: 10px; margin-right: 40px;"href="index.html"><img style="padding: 0; margin: 0;" width="80px" src="Images/Logo.webp" alt="Logo"></a><a class="NavButton" href="Search.html" style="margin-right: 10px;">Search</a><a class="NavButton" href="EOTS/index.html">EOTS</a><a style="margin-left:auto; margin-right:10px;" id="SigninButton" class="NavButton" href="signin.html">Sign In</a><a style="margin-right:20px;" id="SignupButton" class="NavButton" href="signup.html">Sign Up</a><a style="margin-left:auto; margin-right:20px; display: none;" id="AccountButton" class="NavButton" href="account.html">Account</a><a style="margin-right:20px; display: none;" id="ServiceButton" class="NavButton" href="services.html">Services</a>`;
     await AdjustNavText();
     return true;
   } catch (error) {
@@ -31,7 +31,7 @@ async function LoadFooter() {
   try {
     document.getElementById(
       "footer"
-    ).innerHTML = `<p style="margin-left: 10px; margin-bottom: 0; width:100vw;">All data is provided by the <a href="https://weather.gov">National Weather Service</a></p><p style="margin-left:10px; margin-top:5px; width:100vw;" xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/Programer-Turtle/WeatherSystem">Weather System</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/Weather-System-LLC">Weather System LLC</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p><div style="flex-basis: 100%; display:flex; align-content: center;"><a style="margin-right: 10px; margin-left: 10px;" href="https://github.com/Programer-Turtle?tab=repositories"><img src="https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg" alt="GitHub Logo" width="40px" height="40px"></a><a style="margin-right: 10px;" href="https://www.facebook.com/WeatherSystemMain/"><img src="https://static.xx.fbcdn.net/rsrc.php/yT/r/aGT3gskzWBf.ico" alt="Facebook Logo" width="40px" height="40px"></a></div>`;
+    ).innerHTML = `<p style="margin-left: 10px; margin-bottom: 0; width: 100vw">All data is provided by the <a href="https://weather.gov">National Weather Service</a></p><p style="margin-left:10px; margin-top:5px; width:100vw;"><a href="https://github.com/Weather-System-LLC/WeatherSystem">Weather System</a> is brought to you by <a href="https://github.com/Weather-System-LLC">Weather System LLC</a></p><div style="flex-basis: 100%; display: flex; align-content: center"><a style="margin-right: 10px; margin-left: 10px" href="https://github.com/Programer-Turtle?tab=repositories"><img src="https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg" alt="GitHub Logo" width="40px" height="40px" /></a><a style="margin-right: 10px" href="https://www.facebook.com/WeatherSystemMain/"><img src="https://static.xx.fbcdn.net/rsrc.php/yT/r/aGT3gskzWBf.ico" alt="Facebook Logo" width="40px" height="40px" /></a></div>`;
     return true;
   } catch (error) {
     console.error(error);
